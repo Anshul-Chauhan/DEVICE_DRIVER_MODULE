004c87f906261247fbb647e1ac7ae0945b74055f
Anshul <anshulchauhan1807@gmail.com>
2022-01-16 19:42:43 +0530

    Assignment on Wait Queues

---
+++ 07_wait_queue/Ques_1/app2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <fcntl.h>
+
+int main()
+{
+    int fd;
+    char Ubuf[] = "Data from application 2, written successfully";
+
+    fd = open("/dev/WaitCharDevice", O_RDWR, 0777);
+    if(fd < 0)
+    {
+        printf("Error opening Device.\n");
+        exit(1);
+    }
+    
+    write(fd, Ubuf, sizeof(Ubuf));
+    printf("Data from application 2 has written into the kernel buffer successfully.\n");
+
+    close(fd);
+    return 0;
+}
+
---
+++ 07_wait_queue/Ques_2/app3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <fcntl.h>
+
+int main()
+{
+    int fd;
+    char Ubuf[] = "Data from application 3, written successfully";
+
+    fd = open("/dev/WaitCharDevice1", O_RDWR, 0777);
+    if(fd < 0)
+    {
+        printf("Error opening Device.\n");
+        exit(1);
+    }
+    printf("You can press 'ctrl+c' to kill the process as this wait queue is interruptible.\n");
+    write(fd, Ubuf, sizeof(Ubuf));
+    printf("Data from application 3 has written into the kernel buffer successfully.\n");
+
+    close(fd);
+    return 0;
+}
+
